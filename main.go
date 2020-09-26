9dff8fa89038f7de1aaf0e9639f6bd669a33d059
Kazi Jawad <kazi.a.jawad@gmail.com>
2020-09-26 12:44:10 -0400

    Add additional routes for testing

--- main.go
+++ main.go
@@ -17,9 +17,23 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "To get in touch, please send an email to <a href=\"mailto:support@photoshoot.com\">support@photoshoot.com</a>")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "<h1>FAQ</h1><h3>What is this?</h3><h5>This is a web app written in Go.</h5>")
+}
+
+func error(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(w, "<h1>Woops, this page doesn't exist!</h1>")
+}
+
 func main() {
+	var h http.Handler = http.HandlerFunc(error)
 	r := mux.NewRouter()
+	r.NotFoundHandler = h
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":3000", r)
 }
