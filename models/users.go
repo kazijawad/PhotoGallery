d0fd90e279adb2032a9008a49b3d24e1c47d1d07
Kazi Jawad <kazi.a.jawad@gmail.com>
2020-11-21 18:53:23 -0500

    Add UPDATE and DELETE to Users

--- models/users.go
+++ models/users.go
@@ -11,6 +11,10 @@ var (
 	// ErrNotFound is returned when a resource cannot be found
 	// in the database.
 	ErrNotFound = errors.New("models: resource not found")
+
+	// ErrInvalidID is returned when an invalid ID is provided
+	// to a method like Delete.
+	ErrInvalidID = errors.New("models: ID provided was invalid")
 )
 
 type User struct {
@@ -71,6 +75,21 @@ func (us *UserService) ByEmail(email string) (*User, error) {
 // like the ID, CreatedAt, and UpdatedAt fields.
 func (us *UserService) Create(user *User) error {
 	return us.db.Create(user).Error
+}
+
+// Update will update the provided user with all of the data
+// in the provided user object.
+func (us *UserService) Update(user *User) error {
+	return us.db.Save(user).Error
+}
+
+// Delete will delete the user with the provided ID
+func (us *UserService) Delete(id uint) error {
+	if id == 0 {
+		return ErrInvalidID
+	}
+	user := User{Model: gorm.Model{ID: id}}
+	return us.db.Delete(&user).Error
 }
 
 // Close will end the UserService database connection
