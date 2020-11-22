38d2daba2671a8601c7bfd1bfeba9d0af9e9fd7e
Kazi Jawad <kazi.a.jawad@gmail.com>
2020-11-21 19:15:04 -0500

    Setup User Service

--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/kazijawad/Photoshoot/controllers"
+	"github.com/kazijawad/Photoshoot/models"
+)
+
+const (
+	host     = "ec2-54-157-88-70.compute-1.amazonaws.com"
+	port     = 5432
+	user     = "czbyfpbnovxxsj"
+	password = "3ac3643181cc898fa2f80f3b9f77d4cdeb514062a5e3780d30493eaa7f9cb193"
+	dbname   = "d2jd17qkkc675u"
 )
 
 func main() {
+	// Create a DB connection string and then use it to create our model services.
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s", host, port, user, password, dbname)
+
+	us, err := models.NewUserService(psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer us.Close()
+	us.AutoMigrate()
+
 	staticC := controllers.NewStatic()
 	usersC := controllers.NewUsers()
 
