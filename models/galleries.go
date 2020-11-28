9c7eaec738044a4f32ffbd93d3c197e72404cc4e
Kazi Jawad <kazi.a.jawad@gmail.com>
2020-11-27 23:10:22 -0500

    add galleries validation layer for create

--- models/galleries.go
+++ models/galleries.go
@@ -2,6 +2,11 @@ package models
 
 import "github.com/jinzhu/gorm"
 
+const (
+	ErrUserIDRequired modelError = "models: user ID is required"
+	ErrTitleRequired  modelError = "models: title is required"
+)
+
 // Gallery represents the galleries table in our DB
 // and is mostly a container resource composed of images.
 type Gallery struct {
@@ -36,10 +41,47 @@ type galleryValidator struct {
 	GalleryDB
 }
 
+func (gv *galleryValidator) Create(gallery *Gallery) error {
+	err := runGalleryValFns(
+		gallery,
+		gv.userIDRequired,
+		gv.titleRequired,
+	)
+	if err != nil {
+		return err
+	}
+	return gv.GalleryDB.Create(gallery)
+}
+
+func (gv *galleryValidator) userIDRequired(g *Gallery) error {
+	if g.UserID <= 0 {
+		return ErrUserIDRequired
+	}
+	return nil
+}
+
+func (gv *galleryValidator) titleRequired(g *Gallery) error {
+	if g.Title == "" {
+		return ErrTitleRequired
+	}
+	return nil
+}
+
 type galleryGorm struct {
 	db *gorm.DB
 }
 
 func (gg *galleryGorm) Create(gallery *Gallery) error {
 	return gg.db.Create(gallery).Error
+}
+
+type galleryValFn func(*Gallery) error
+
+func runGalleryValFns(gallery *Gallery, fns ...galleryValFn) error {
+	for _, fn := range fns {
+		if err := fn(gallery); err != nil {
+			return err
+		}
+	}
+	return nil
 }
