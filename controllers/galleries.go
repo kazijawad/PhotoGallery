739ab529c8ea502fbfcb0926514c86a4537417b9
Kazi Jawad <kazi.a.jawad@gmail.com>
2020-11-27 23:03:03 -0500

    process gallery form

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -1,9 +1,16 @@
 package controllers
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/kazijawad/PhotoGallery/models"
 	"github.com/kazijawad/PhotoGallery/views"
 )
+
+type GalleryForm struct {
+	Title string `schema:"title"`
+}
 
 type Galleries struct {
 	New *views.View
@@ -15,4 +22,25 @@ func NewGalleries(gs models.GalleryService) *Galleries {
 		New: views.NewView("bootstrap", "galleries/new"),
 		gs:  gs,
 	}
+}
+
+func (g *Galleries) Create(w http.ResponseWriter, r *http.Request) {
+	var vd views.Data
+	var form GalleryForm
+	if err := parseForm(r, &form); err != nil {
+		vd.SetAlert(err)
+		g.New.Render(w, vd)
+		return
+	}
+
+	gallery := models.Gallery{
+		Title: form.Title,
+	}
+	if err := g.gs.Create(&gallery); err != nil {
+		vd.SetAlert(err)
+		g.New.Render(w, vd)
+		return
+	}
+
+	fmt.Fprintln(w, gallery)
 }
--- models/galleries.go
+++ models/galleries.go
@@ -6,8 +6,8 @@ import "github.com/jinzhu/gorm"
 // and is mostly a container resource composed of images.
 type Gallery struct {
 	gorm.Model
-	UserID uint `gorm:"not_null;index"`
-	Title  uint `gorm:"not_null"`
+	UserID uint   `gorm:"not_null;index"`
+	Title  string `gorm:"not_null"`
 }
 
 type GalleryService interface {
