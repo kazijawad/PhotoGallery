fd668b34c05f927beda41ce0efebbc343889ac67
Kazi Jawad <kazi.a.jawad@gmail.com>
2020-11-28 19:50:10 -0500

    add edit gallery action

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -21,6 +21,7 @@ type GalleryForm struct {
 type Galleries struct {
 	New      *views.View
 	ShowView *views.View
+	EditView *views.View
 	gs       models.GalleryService
 	r        *mux.Router
 }
@@ -29,6 +30,7 @@ func NewGalleries(gs models.GalleryService, r *mux.Router) *Galleries {
 	return &Galleries{
 		New:      views.NewView("bootstrap", "galleries/new"),
 		ShowView: views.NewView("bootstrap", "galleries/show"),
+		EditView: views.NewView("bootstrap", "galleries/edit"),
 		gs:       gs,
 		r:        r,
 	}
@@ -62,16 +64,48 @@ func (g *Galleries) Create(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, url.Path, http.StatusFound)
 }
 
-// Show GET /galleries/:id
+// Show is a GET /galleries/:id
 func (g *Galleries) Show(w http.ResponseWriter, r *http.Request) {
+	gallery, err := g.galleryByID(w, r)
+	if err != nil {
+		return
+	}
+	var vd views.Data
+	vd.Yield = gallery
+	g.ShowView.Render(w, vd)
+}
+
+// Edit is a GET /galleries/:id/edit
+func (g *Galleries) Edit(w http.ResponseWriter, r *http.Request) {
+	gallery, err := g.galleryByID(w, r)
+	if err != nil {
+		return
+	}
+	user := context.User(r.Context())
+	if gallery.UserID != user.ID {
+		http.Error(w, "Yo do not have permission to edit this gallery.", http.StatusForbidden)
+		return
+	}
+	var vd views.Data
+	vd.Yield = gallery
+	g.EditView.Render(w, vd)
+}
+
+// galleryByID will parse the "id" variable from the
+// request path using gorilla/mux and then use that ID to
+// retrieve the gallery from the GalleryService
+//
+// galleryByID will return an error if one occurs, but it
+// will also render the error with an http.Error function
+// call, so you do not need to.
+func (g *Galleries) galleryByID(w http.ResponseWriter, r *http.Request) (*models.Gallery, error) {
 	vars := mux.Vars(r)
 	idStr := vars["id"]
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
 		http.Error(w, "Invalid Gallery ID", http.StatusNotFound)
-		return
+		return nil, err
 	}
-
 	gallery, err := g.gs.ByID(uint(id))
 	if err != nil {
 		switch err {
@@ -80,10 +114,7 @@ func (g *Galleries) Show(w http.ResponseWriter, r *http.Request) {
 		default:
 			http.Error(w, "Whoops! Something went wrong.", http.StatusInternalServerError)
 		}
-		return
+		return nil, err
 	}
-
-	var vd views.Data
-	vd.Yield = gallery
-	g.ShowView.Render(w, vd)
+	return gallery, nil
 }
