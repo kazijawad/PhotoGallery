6145256e2ff834f690c2437c96d4133930069603
Kazi Jawad <kazi.a.jawad@gmail.com>
2020-12-12 22:24:37 -0500

    render images with columns

--- models/galleries.go
+++ models/galleries.go
@@ -19,6 +19,20 @@ type Gallery struct {
 	Images []string `gorm:"-"`
 }
 
+// ImageSplitN splits images from a gallery into a
+// two-dimensional slice.
+func (g *Gallery) ImageSplitN(n int) [][]string {
+	ret := make([][]string, n)
+	for i := 0; i < n; i++ {
+		ret[i] = make([]string, 0)
+	}
+	for i, img := range g.Images {
+		bucket := i % n
+		ret[bucket] = append(ret[bucket], img)
+	}
+	return ret
+}
+
 // GalleryService is a set of methods used to manipulate and
 // work with the gallery model.
 type GalleryService interface {
