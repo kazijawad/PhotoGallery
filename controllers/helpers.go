40d4277724bf0e854d58055aa396bf0d6bc83f80
Kazi Jawad <kazi.a.jawad@gmail.com>
2020-12-13 21:29:20 -0500

    persist and prefill form data

--- controllers/helpers.go
+++ controllers/helpers.go
@@ -2,18 +2,30 @@ package controllers
 
 import (
 	"net/http"
+	"net/url"
 
 	"github.com/gorilla/schema"
 )
+
+func parseURLParams(r *http.Request, dst interface{}) error {
+	if err := r.ParseForm(); err != nil {
+		return err
+	}
+	return parseValues(r.Form, dst)
+}
 
 // parseForm uses gorilla/schema to decode form data.
 func parseForm(r *http.Request, dst interface{}) error {
 	if err := r.ParseForm(); err != nil {
 		return err
 	}
+	return parseValues(r.PostForm, dst)
+}
+
+func parseValues(values url.Values, dst interface{}) error {
 	dec := schema.NewDecoder()
 	dec.IgnoreUnknownKeys(true)
-	if err := dec.Decode(dst, r.PostForm); err != nil {
+	if err := dec.Decode(dst, values); err != nil {
 		return err
 	}
 	return nil
--- controllers/users.go
+++ controllers/users.go
@@ -52,7 +52,9 @@ func NewUsers(us models.UserService) *Users {
 //
 // GET /signup
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
-	u.NewView.Render(w, r, nil)
+	var form SignupForm
+	parseURLParams(r, &form)
+	u.NewView.Render(w, r, form)
 }
 
 // Create is used to process the signup form when a user
@@ -62,6 +64,7 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	var vd views.Data
 	var form SignupForm
+	vd.Yield = &form
 
 	if err := parseForm(r, &form); err != nil {
 		vd.SetAlert(err)
